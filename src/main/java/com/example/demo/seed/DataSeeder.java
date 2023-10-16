2473bfaee8cafbee77a179bb00e8636cf10c8dce
sonalpriya <sonalpriya172@gmail.com>
2023-10-16 12:07:22 +0530

    data seeding using mongo

--- src/main/java/com/example/demo/seed/DataSeeder.java
+++ src/main/java/com/example/demo/seed/DataSeeder.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.util.ResourceUtils;
 import java.io.File;
 import java.util.List;
 
